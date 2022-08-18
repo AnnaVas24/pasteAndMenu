f40b7ce8ee737a6b08551eb32a114cdd2bf473ba
Anna <anna.vasi4ko@icloud.com>
2022-08-18 17:35:53 +0300

    Initial Commit

---
+++ pasteAndMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pasteAndMenu
+//
+//  Created by Vasichko Anna on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
