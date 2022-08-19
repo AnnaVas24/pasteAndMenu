b00bf74826ae5df7dc06967a0a2b4ce77c43ec57
Anna <anna.vasi4ko@icloud.com>
2022-08-19 12:41:21 +0300

    paste control project

--- pasteAndMenu/ViewController.swift
+++ pasteAndMenu/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMenu()
     }
-
-
+    
+    private func setupMenu() {
+        let menuElements: [UIAction] = [
+            UIAction(
+                title: "Copy",
+                image: UIImage(systemName: "doc.on.doc"),
+                handler: { _ in
+                    
+                }
+            ),
+            UIAction(
+                title: "Delete",
+                image: UIImage(systemName: "trash"),
+                handler: { _ in
+                    
+                }
+            ),
+            UIAction(
+                identifier: UIAction.Identifier.paste,
+                handler: { _ in
+                    let pasteboardContent = UIPasteboard.general.string
+                    if let pasteboardContent {
+                        self.mainTextView.text = pasteboardContent
+                    }
+                }
+            )
+        ]
+        
+        let demoMenu = UIMenu(children: menuElements)
+        let plusButton = UIBarButtonItem(image: UIImage(systemName: "plus.circle"), menu: demoMenu)
+        navigationItem.rightBarButtonItem = plusButton
+        
+    }
 }
 
